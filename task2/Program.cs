b79528349c96cc30a6ebfc1e49beb5c73dd303ba
Anastasiya <anastasiyaosnov.mail.ru>
2022-12-31 21:05:34 +0300

    tasks

---
+++ task2/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 2. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Ряд > 1
+// Колонка > 7
+// 1, 7 -> такого числа в массиве нет
+
+int Promt(string message)
+{
+    Console.WriteLine($"{message}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillMatrix(int[,] matr, int max, int min)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+}
+
+
+int[,] UserMatrix = new int[3, 3];
+FillMatrix(UserMatrix, 100, -100);
+PrintMatrix(UserMatrix);
+
+int UserRow = Promt("Введите позицию ряда > ");
+int UserColumn = Promt("Введите позицию колонки > ");
+
+if (UserRow <= UserMatrix.GetLength(0) && UserColumn <= UserMatrix.GetLength(1))
+{
+    Console.WriteLine($"Значение элемента массива, расположенного на позиции {UserRow}, {UserColumn} -> {UserMatrix[UserRow - 1, UserColumn - 1]}");
+}
+else
+{
+    Console.WriteLine($"{UserRow}, {UserColumn} -> такого элемента в массиве нет");
+}
---
+++ task3/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int Promt(string message)
+{
+    Console.WriteLine($"{message}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillMatrix(int[,] matr, int min, int max)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(min, max);
+        }
+    }
+}
+
+void AverageColumns(int[,] matrix)
+{
+    int num = matrix[0, 0];
+
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        double sum1 = 0;
+        double count = 0;
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            sum1 += matrix[j, i];
+            count++;
+        }
+
+        double average = sum1 / count;
+        Console.Write($"{average:f1} ");
+
+    }
+}
+
+
+
+int rows = Promt("Сколько будет в матрице строк?");
+int columns = Promt("Сколько будет в матрице столбцов?");
+int UserMin = Promt("Введите минимальное значение элемента матрицы");
+int UserMax = Promt("Введите максимальное значение элемента матрицы");
+
+int[,] UserMatrix = new int[rows, columns];
+FillMatrix(UserMatrix, UserMin, UserMax);
+PrintMatrix(UserMatrix);
+Console.WriteLine("Среднее арифметическое каждого столбца: ");
+AverageColumns(UserMatrix);
